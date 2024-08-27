fc0393374a4d9695e1a0bb93fb9b7a2ad881a66b
Hossein Mohammadzadeh <4hmzjob@gmail.com>
2024-08-28 02:16:09 +0330

    V0.2 Add Mongoose And Conection Function

---
+++ src/utils/connectDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    if (mongoose.connection.readyState) return;
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected To DB");
+  } catch (error) {
+    console.log("Can Not Connect To DB \n", error);
+  }
+};
+
+export default connectDB;
